8a77a5213b97a2acc372360493fc3a62db03ffdb
gmunoz97 <gerardo@enviopack.com>
2022-07-13 17:06:18 -0300

    Se agrega seccion de preparacion de receta

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
                 alt="brownie-imagen"
                 style={{ width: '200px' }}
             />
+            <h3>Ingredientes:</h3>
             <ul>
                 <li>75gr de manteca</li>
                 <li>150gr de chocolate</li>
@@ -14,6 +15,14 @@ function App() {
                 <li>1 taza de azúcar</li>
                 <li>100gr de harina</li>
             </ul>
+            <h3>Preparación:</h3>
+            <ol>
+                <li>Calentar la manteca y el chocolate a fuego bien bajo</li>
+                <li>Batir los 2 huevos con el azúcar hasta que queden bien blancos</li>
+                <li>Agregar el chocolate derretido y batir hasta que esté integrado</li>
+                <li>Sumar el harina tamizada e integrar todo</li>
+                <li>Colocar en una placa y cocinar al horno fuerte (200-220°) por 20 minutos.</li>
+            </ol>
         </div>
     );
 }
