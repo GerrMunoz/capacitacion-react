8d9097f4ea6a7b2093fc27894217c18827da609b
Gerardo Muñoz <gmunoznieto@frba.utn.edu.ar>
2022-07-13 15:22:50 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+    return (
+        <div>
+            <h1>Receta brownies</h1>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/6/68/Chocolatebrownie.JPG"
+                alt="brownie-imagen"
+                style={{ width: '200px' }}
+            />
+            <ul>
+                <li>75gr de manteca</li>
+                <li>150gr de chocolate</li>
+                <li>2 huevos</li>
+                <li>1 taza de azúcar</li>
+                <li>100gr de harina</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
