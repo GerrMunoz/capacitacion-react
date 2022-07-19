7c537cd1e41850430777f73f7b9162262dbb9057
gmunoz97 <gerardo@enviopack.com>
2022-07-19 16:20:41 -0300

    Clase 3 - codigo App con muchos atributos styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,120 @@
+import './styles.css';
+
 function App() {
     return (
         <div>
-            <h1>Receta brownies</h1>
-            <img
-                src="https://upload.wikimedia.org/wikipedia/commons/6/68/Chocolatebrownie.JPG"
-                alt="brownie-imagen"
-                style={{ width: '200px' }}
-            />
-            <h3>Ingredientes:</h3>
-            <ul>
-                <li>75gr de manteca</li>
-                <li>150gr de chocolate</li>
-                <li>2 huevos</li>
-                <li>1 taza de azúcar</li>
-                <li>100gr de harina</li>
-            </ul>
-            <h3>Preparación:</h3>
-            <ol>
-                <li>Calentar la manteca y el chocolate a fuego bien bajo</li>
-                <li>Batir los 2 huevos con el azúcar hasta que queden bien blancos</li>
-                <li>Agregar el chocolate derretido y batir hasta que esté integrado</li>
-                <li>Sumar el harina tamizada e integrar todo</li>
-                <li>Colocar en una placa y cocinar al horno fuerte (200-220°) por 20 minutos.</li>
-            </ol>
+            <h1
+                style={{
+                    letterSpacing: '-0.2px',
+                    color: 'maroon',
+                    fontFamily: 'sans-serif'
+                }}
+            >
+                Receta brownies
+            </h1>
+            <div>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/6/68/Chocolatebrownie.JPG"
+                    alt="brownie-imagen"
+                    style={{ width: '200px', borderRadius: '25%', border: '2px solid chocolate' }}
+                />
+            </div>
+            <div style={{ backgroundColor: 'navajoWhite', borderRadius: '10px', padding: '20px' }}>
+                <h3 style={{ fontFamily: 'sans-serif', color: 'maroon', margin: '0px' }}>Ingredientes:</h3>
+                <ul>
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square'
+                        }}
+                    >
+                        75gr de manteca
+                    </li>
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square'
+                        }}
+                    >
+                        150gr de chocolate
+                    </li>
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square'
+                        }}
+                    >
+                        2 huevos
+                    </li>
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square'
+                        }}
+                    >
+                        1 taza de azúcar
+                    </li>
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square'
+                        }}
+                    >
+                        100gr de harina leudante
+                    </li>
+
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square'
+                        }}
+                    >
+                        Chips de chocolate (opcional)
+                    </li>
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square'
+                        }}
+                    >
+                        Esencia de vainilla (opcional)
+                    </li>
+                    <li
+                        style={{
+                            fontFamily: 'sans-serif',
+                            letterSpacing: '-0.2px',
+                            fontSize: '18px',
+                            margin: '2px 0',
+                            listStyleType: 'square',
+                            fontWeight: 'bold',
+                            color: 'chocolate'
+                        }}
+                    >
+                        Ingrediente secreto de Gera
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
