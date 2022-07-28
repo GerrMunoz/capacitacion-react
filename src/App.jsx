e80b3fc8023c3fbc2e7d263f7ab80e78748766a4
gmunoz97 <gerardo@enviopack.com>
2022-07-28 14:13:41 -0300

    Se agrega componente Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,32 @@
+import Box from './components/Box/Box';
 import Title from './components/Title/Title';
 import Image from './components/Image/Image';
-import './styles.css';
 import List from './components/List/List';
 import ListItem from './components/ListItem/ListItem';
+import './styles.css';
 
 function App() {
     return (
-        <div>
+        <Box padding="16px">
             <Title>Receta brownies</Title>
-            <h1>Receta brownies</h1>
+
             <Image url="https://upload.wikimedia.org/wikipedia/commons/6/68/Chocolatebrownie.JPG" alt="brownie" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/6/68/Chocolatebrownie.JPG" alt="brownie-imagen" />
-            <div>
-                <Title isSmall={true}>Ingredientes</Title>
-                <h3>Ingredientes</h3>
-                <ul>
-                    <li>100gr de manteca</li>
-                    <li>150gr de chocolate</li>
-                    <li>2 huevos</li>
-                    <li>1 taza de azúcar</li>
-                    <li>100gr de harina</li>
-                    <li className="ingrediente-opcional">Chips de chocolate (opcional)</li>
-                    <li className="ingrediente-opcional">Esencia de vainilla (opcional)</li>
-                    <li className="ingrediente-secreto">Ingrediente secreto de Gera</li>
-                </ul>
+
+            <Box backgroundColor="navajowhite" borderRadius="10px" padding="16px" marginTop="8px">
+                <Title isSmall>Ingredientes</Title>
+
                 <List>
                     <ListItem>75gr de manteca</ListItem>
                     <ListItem>150gr de chocolate</ListItem>
                     <ListItem>2 huevos</ListItem>
                     <ListItem>1 taza de azúcar</ListItem>
                     <ListItem>100gr de harina leudante</ListItem>
+                    <ListItem isImportant={true}>Ingrediente secreto de Gera</ListItem>
                     <ListItem isOptional={true}>Chips de chocolate (opcional)</ListItem>
                     <ListItem isOptional={true}>Esencia de vainilla (opcional)</ListItem>
-                    <ListItem isImportant={true}>Ingrediente secreto de Gera</ListItem>
                 </List>
-            </div>
-        </div>
+            </Box>
+        </Box>
     );
 }
 
